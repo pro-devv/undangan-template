8b8557e550d52a918be81d9db4aba75c379b5047
Rifjan29 <rjndla@gmail.com>
2022-01-09 23:52:32 +0700

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+// importing AOS
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(AOS.init());
+
+app.mount('#app')
